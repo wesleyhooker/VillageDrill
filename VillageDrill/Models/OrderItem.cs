4c8d515c93df3f941a22d411c0661bbcabd49882
jbnguyen215 <jbnguyen9889@gmail.com>
2019-07-08 21:35:32 -0600

    Create OrderItem

---
+++ VillageDrill/Models/OrderItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VillageDrill.Models
+{
+    public class OrderItem
+    {
+        [Key]
+        public int OrderItemID { get; set; }
+
+        public int ItemID { get; set; }
+
+        public int PurchaseOrderID {get; set;}
+
+        public string VendorSKU { get; set; }
+
+        public double Price { get; set; }
+
+        public int QuantityOrdered { get; set; }
+
+        public DateTimeOffset DateDelivered { get; set; }
+
+        public string LastModifiedBy { get; set; }
+
+        public DateTimeOffset LastModifiedDate { get; set; }
+ 
+    }
+}
