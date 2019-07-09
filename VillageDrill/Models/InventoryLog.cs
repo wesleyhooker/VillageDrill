2a0235daddf28b1dfd5417788acbc0e596794a63
jbnguyen215 <jbnguyen9889@gmail.com>
2019-07-08 21:57:24 -0600

    Create InventoryLog model

---
+++ VillageDrill/Models/InventoryLog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VillageDrill.Models
+{
+    public class InventoryLog
+    {
+        [Key]
+        public int LogID { get; set; }
+
+        public int ItemID { get; set; }
+
+        public int PreviousQty { get; set; }
+
+        public int NewQty { get; set; }
+
+        public bool Reconciled { get; set; }
+        
+        public string LastModifiedBy { get; set; }
+
+        public DateTimeOffset LastModifieDate { get; set; }
+
+    }
+}
