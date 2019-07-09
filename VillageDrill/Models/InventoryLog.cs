373e5a018c2aef0cf0202020ee19fa31dbdf2431
jbnguyen215 <jbnguyen9889@gmail.com>
2019-07-08 22:15:10 -0600

    Initial Commit

--- VillageDrill/Models/InventoryLog.cs
+++ VillageDrill/Models/InventoryLog.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,6 +14,9 @@ namespace VillageDrill.Models
 
         public int ItemID { get; set; }
 
+        [ForeignKey("ItemID")]
+        public virtual Item Item { get; set; }
+ 
         public int PreviousQty { get; set; }
 
         public int NewQty { get; set; }
--- VillageDrill/Models/OrderItem.cs
+++ VillageDrill/Models/OrderItem.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,9 +12,17 @@ namespace VillageDrill.Models
         [Key]
         public int OrderItemID { get; set; }
 
+        [Display(Name = "Item (Part)")]
         public int ItemID { get; set; }
 
+        [ForeignKey("ItemID")]
+        public virtual Item Item { get; set; }
+
+
         public int PurchaseOrderID {get; set;}
+
+        [ForeignKey("PurchaseOrderID")]
+        public virtual PurchaseOrder PurchaseOrder { get; set; }
 
         public string VendorSKU { get; set; }
 
