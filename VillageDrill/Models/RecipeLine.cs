b451fa4f8e8db383b21416df5ce0b6827481693e
Wesley Hooker <wesleyhooker@outlook.com>
2019-07-08 21:55:26 -0600

    Added AssemblyRecipe, Item and RecipeLine

---
+++ VillageDrill/Models/RecipeLine.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VillageDrill.Models
+{
+    public class RecipeLine
+    {
+        [Key]
+        public int RecipeLineID { get; set; }
+        [DisplayName("Required Item Quantity")]
+        [Required]
+        public int RequiredItemQuantity { get; set; }
+        [DisplayName("Last Modified by")]
+        public string LastModifiedBy { get; set; }
+        [DisplayName("Last Modified on")]
+        public DateTime LastModifiedDate { get; set; }
+
+        //Foreign Key
+        [Display(Name = "Item (Part)")]
+        public int ItemID { get; set; }
+
+        [ForeignKey("ItemID")]
+        public virtual Item Item { get; set; }
+
+        [Display(Name = "Assembly Recipe")]
+        public int AssemblyRecipeID { get; set; }
+
+        [ForeignKey("AssemblyRecipeID")]
+        public virtual AssemblyRecipe AssemblyRecipe { get; set; }
+    }
+}
