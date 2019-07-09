6821a68898a96d0241c95997c24ab7f19a7baf7f
inbranlable <inbranlable@hotmail.com>
2019-07-08 21:54:23 -0600

    Added AssemblyHistory, Measure, PurchaseOrder models

---
+++ VillageDrill/Models/AssemblyHistory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace VillageDrill.Models
+{
+    public class AssemblyHistory
+    {
+        [Display(Name = "History ID")]
+        public int AssemblyHistoryID { get; set; }
+
+        [Display(Name = "Item ID")]
+        public int ItemID { get; set; }
+
+        [Required]
+        [Display(Name = "Date Assembled")]
+        public DateTime AssemblyDate { get; set; }
+
+        [Required]
+        [Display(Name = "Quantity")]
+        public int QtyAssembled { get; set; }
+
+        [Display(Name = "Additional Notes")]
+        public string Notes { get; set; }
+
+        [Display(Name = "Modified By")]
+        public string LastModifiedBy { get; set; }
+
+        [Display(Name = "Modified On")]
+        public DateTime LastModifiedDate { get; set; }
+
+        [ForeignKey("ItemID")]
+        public virtual Item Item { get; set; }
+    }
+}
---
+++ VillageDrill/Models/PurchaseOrder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace VillageDrill.Models
+{
+    public class PurchaseOrder
+    {
+        [Display(Name = "Purchase Order ID")]
+        public int PurchaseOrderID { get; set; }
+        [Display(Name = "Vendor ID")]
+        public int VendorID { get; set; }
+        [Required]
+        [Display(Name = "Date Ordered")]
+        public DateTime DateOrdered { get; set; }
+        [Required]
+        [Display(Name = "Vendor PO")]
+        public String VendorPO { get; set; }
+        [Display(Name = "Last Modified By")]
+        public String LastModifiedBy { get; set; }
+        [Display(Name = "Last Modified On")]
+        public DateTime LastModifiedDate { get; set; }  
+
+        [ForeignKey("VendorID")]
+        public virtual Vendor Vendor { get; set; }
+    }
+}
