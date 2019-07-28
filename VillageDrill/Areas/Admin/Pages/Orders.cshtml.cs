8a8f36043eba62acee3637ce9f390fef5e7f5219
Brandon Burke <brandonburke93@outlook.com>
2019-07-27 22:14:46 -0600

    Update order page to work with database

--- VillageDrill/Areas/Admin/Pages/Orders.cshtml.cs
+++ VillageDrill/Areas/Admin/Pages/Orders.cshtml.cs
@@ -4,13 +4,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using VillageDrill.Models.Objects;
+using VillageDrill.Models.Repositories;
 
 namespace VillageDrill.Areas.Admin.Pages
 {
     public class OrdersModel : PageModel
     {
-        public void OnGet()
+        private IRepositoryWrapper _repositoryWrapper;
+        public List<PurchaseOrder> Orders { get; set; }
+
+        public OrdersModel(IRepositoryWrapper repositoryWrapper)
         {
+            _repositoryWrapper = repositoryWrapper;
+        }
+
+        public async Task OnGet()
+        {
+            var orders = _repositoryWrapper.PurchaseOrder.FindAll();
+            Orders = await orders.ToListAsync();
         }
     }
 }
